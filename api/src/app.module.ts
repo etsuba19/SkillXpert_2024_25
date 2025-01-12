d4131a84a1072ebe40355eca79269ce80caad431
etsuba19 <etsuba24@gmail.com>
2025-01-12 21:49:10 +0300

    Implement authentication and authorization

---
+++ api/src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { CourseModule } from './course/course.module';
+import { EnrollmentModule } from './enrollment/enrollement.module';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: "elearning.sqlite",
+      autoLoadEntities: true,
+      synchronize: true,      
+    }),
+    UserModule,
+    AuthModule,
+    CourseModule,
+    EnrollmentModule,
+    
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
